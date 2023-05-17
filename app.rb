17a2136239d772c2823f197c9c48fdd6e7fc8c6a
Antonio <antonio18casta@aol.com>
2023-05-17 17:36:41 -0400

    commit tree b175af51c7ed4a2e9910daa038902baf44e6457d

--- app.rb
+++ app.rb
@@ -3,21 +3,29 @@
 #     { id: 3, hello: "Moon" }, 
 #     { id: 4, hello: "Learner" }]
 
-# p [1,2,3]
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
 # puts "Hello World!"
-# print "Pass this test, please."
+# puts "Hello World!"
+# print "Hello Ruby!"
+# print "Hello Ruby!"
+# print "Hello Ruby!"
+
+# puts [1, 2, 3]
+# # print "Pass this test, please."
 # p [1,2,3]
 
-# def add
+# # def add
     
-# end
-# # 1 + 1
-# puts add + add
+# # end
+# # # 1 + 1
+# # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# # def add num1, num2
+# #     puts num1
+# #     puts num2
+# #     num1 + num2
+# # end
 
-puts add(1,3) + add(2,2)
+# # puts add(1,3) + add(2,2)
